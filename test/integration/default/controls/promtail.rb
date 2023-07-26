e175a5262df1240f66a3f84111016099a45171bd
Marcos Essindi <marcessindi@me.com>
2023-07-26 14:39:11 +0200

    Add prvsn_promtail role

---
+++ test/integration/default/controls/promtail.rb
@@ -0,0 +1,19 @@
+control "Promtail" do
+  title ""
+
+  describe service("promtail") do
+    it { is_expected.to be_enabled }
+    it { is_expected.to be_running }
+  end
+
+  describe file("/etc/promtail/config.yml") do
+    its(:content) do
+      should match(%r{http://localhost:3100/loki/api/v1/push})
+    end
+  end
+
+  describe user("promtail") do
+    it { should exist }
+    its("groups") { should eq %w[nogroup root adm systemd-journal docker] }
+  end
+end
