9c052a8b48f7eda1674632075ad12cbd33103a84
Marcos Essindi <marcessindi@me.com>
2023-08-09 09:38:40 +0200

    Use env vars for subdomain and hcloud token

--- test/fixtures/default/main.tf
+++ test/fixtures/default/main.tf
@@ -1,6 +1,37 @@
+locals {
+  stack   = {
+    name = "test_starter_stack"
+    network_cidr = "10.1.0.0/16"
+    zone = "eu-central"
+    ssh_key_name = "default-key"
+    domain = "prvsn.dev"
+
+    servers = [
+      {
+        name = "web-1"
+        type = "cpx11"
+        data_volume_size = 10
+      },
+      {
+        name = "web-2"
+        type = "cpx11"
+      },
+    ],
+    load_balancers = [
+      {
+        type           = "lb11"
+        subdomain      = var.www_subdomain
+        target_port    = 3000
+        target_servers = [
+          "web-1"
+        ]
+      }
+    ]
+  }
+}
 
 module "default" {
   source  = "../../../"
   hcloud_read_token = var.hcloud_read_token
-  stack = var.stack
+  stack = local.stack
 }
--- test/fixtures/default/vars.tf
+++ test/fixtures/default/vars.tf
@@ -2,23 +2,6 @@ variable "hcloud_read_token" {
   type = string
 }
 
-variable "stack" {
-  type = object({
-    name = string
-    zone = string
-    ssh_key_name = string
-    network_cidr = string
-    domain = string
-    servers = list(object({
-      name             = string
-      type             = string
-      data_volume_size = optional(number)
-    }))
-    load_balancers = list(object({
-      type                = string
-      subdomain           = string
-      target_port         = number
-      target_servers      = list(string)
-    }))
-  })
+variable "www_subdomain" {
+  type = string
 }
